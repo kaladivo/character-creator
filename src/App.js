6c74215665661aaa257574b8483dc06737b7b9ef
David Novák <mail@davenov.com>
2024-01-27 16:08:48 +0100

    Solution

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
+import { Background } from "./components/Background";
 
 function App() {
   return (
     <>
+      <Background />
       <CharacterEditor />
       <Footer />
     </>
---
+++ src/components/Background/Background.js
@@ -0,0 +1,8 @@
+import styles from "./Background.module.css";
+export default function Background() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.grayBack} />
+    </div>
+  );
+}
